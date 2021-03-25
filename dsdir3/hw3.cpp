bc03ec2879c1a27dada9ce0d17c25aa65c902927
kutta97 <yhjin04170@gmail.com>
2021-03-26 04:21:49 +0900

    fix hw3a.cpp main

--- dsdir3/hw3.cpp
+++ dsdir3/hw3.cpp
@@ -8,16 +8,16 @@ using namespace std;
 void getdata(istream&, int&, int&);
 void Path(int, int);
 
-int main(int argc, char argv[]) {
+int main(int argc, char* argv[]) {
 	int m, p; // m by p maze
 	if (argc == 1)
 		cerr << "Usage" << argv[0] << " maze_data_file\n";
 	else {
-		if fstream is (argv[1]);
+		ifstream is(argv[1]);
 		if (!is) { cerr << argv[1] << "does not exist\n";
 			return 1; }
-		cout << "For maze datafile [" << argv[1] >> "]\n";
+		cout << "For maze datafile [" << argv[1] << "]\n";
 		getdata(is, m, p); is.close();
-		Patch(m, p);
+		Path(m, p);
 	}
 }
