5bac7bc689143b3e099dd27a900e3f7b35f802d8
kutta97 <yhjin04170@gmail.com>
2021-04-25 23:01:40 +0900

    add hw6.cpp

--- dsdir6/hw6.cpp
+++ dsdir6/hw6.cpp
@@ -9,4 +9,12 @@ int main()
 	IntList il;
 	il.Push_Back(5); il.Push_Back(7); il.Push_Back(9);
 	cout << il;
+	il.Push_Front(4); il.Push_Front(3);
+	cout << il;
+	il.Insert(6); il.Insert(10); il.Insert(2);
+	il.Insert(5); il.Insert(2); il.Insert(10);
+	while (!il.IsEmpty()) {
+		cout << il.Front() << " "; il.Pop_Front();
+	}
+	cout << endl;
 }
