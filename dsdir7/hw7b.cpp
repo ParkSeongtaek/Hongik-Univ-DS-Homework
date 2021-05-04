91890360215043021a5cf235bae13f6211f90527
kutta97 <yhjin04170@gmail.com>
2021-05-05 02:01:28 +0900

    add hw7b.cpp

---
+++ dsdir7/hw7b.cpp
@@ -0,0 +1,19 @@
+// 금속조형디자인과 3학년
+// B743014 양혜진
+
+#include "tree.h"
+
+int main() {
+	BST<string, double> bst;
+	string str;
+	double dval;
+
+	cout << "Enter string & value pairs:\n";
+	while (cin >> str >> dval)
+		tree.Insert(str, dval);
+	cout << endl << "Preorder traversal:	"; bst.Preorder();
+	cout << endl << "Inorder traversal:	"; bst.Inorder();
+	cout << endl << "Postorder traversal:	"; bst.Postorder();
+	cout << endl << "Levelorder traversal:	"; bst.Levelorder();
+	cout << endl;
+}
