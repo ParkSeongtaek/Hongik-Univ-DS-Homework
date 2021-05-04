b49117afef8f5c1c02afd66856572fe0b51f7509
kutta97 <yhjin04170@gmail.com>
2021-05-05 02:10:42 +0900

    add tree.h

---
+++ dsdir7/tree.h
@@ -0,0 +1,97 @@
+// 금속조형디자인과 3학년
+// B743014 양혜진
+
+#ifndef TREE_H
+#define TREE_H
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+template <class K, class E>
+struct Node {
+	Node(K ky, E el, Node<K, E> *left=0, Node<T> *right=0)
+		: key(ky), element(el), leftChild(left), rightChild(right) { }
+	
+	Node<K, E> *leftChild;
+	K key;
+	E element;
+	Node<K, E> *rightChild;
+};
+
+template <class K, class E>
+class BST {
+	public:
+		BST() { root = 0; } // empty tree
+		void Insert(K &newkey, E el) { Insert(root, newkey, el); }
+		void Preorder() { Preorder(root); }
+		void Inorder() { Inorder(root); }
+		void Postorder() { Postorder(root); }
+
+		void Levelorder();
+	private: // helper 함수들
+		void Visit(Node<K, E> *); 
+		void Insert(Node<K, E>* &, K, E); 
+		void Preorder(Node<K, E> *); 
+		void Inorder(Node<K, E> *); 
+		void Postorder(Node<K, E> *);
+
+		Node<K, E> *root;
+};
+
+template <class K, class E>
+void BST<K, E>::Visit(Node<K, E> *ptr)
+	{ cout << ptr->key << ":" << ptr->element << " "; }
+
+template <class K, class E>
+void BST<K, E>::Insert(Node<K, E>* &ptr, K newkey, E el) { // Insert의 helper 함수
+	if (ptr==0) ptr = new Node<K, E>(newkey, el);
+	else if (value < ptr->key) Insert(ptr->leftChild, newkey, el);
+	else if (value > ptr->key) Insert(ptr->rightChild, newkey, el);
+	else ptr->element = el; // Update element
+}
+
+template <class K, class E>
+void BST<K, E>::Preorder(Node<K, E> *currentNode) { // Preorder의 helper 함수
+	if (currentNode) {
+		Visit(currentNode);
+		Preorder(currentNode->leftChild);
+		Preorder(currentNode->rightChild);
+	}
+}
+
+template <class K, class E>
+void BST<K, E>::Inorder(Node<K, E> *currentNode) { // Inorder helper 함수
+	if (currentNode) {
+		Inorder(currentNode->leftChild);
+		Visit(currentNode);
+		Inorder(currentNode->rightChild);
+	}
+}
+
+template <class K, class E>
+void BST<K, E>::Postorder(Node<K, E> *currentNode) { // Postorder의 helper 함수
+	if (currentNode) {
+		Postorder(currentNode->leftChild);
+		Postorder(currentNode->rightChild);
+		Visit(currentNode);
+	}
+}
+
+template <class K, class E>
+void BST<K, E>::Levelorder() { // Preorder의 helper 함수
+	queue<Node<T>*> q; // queue is declared
+	Node<T>* currentNode = root;
+	while (currentNode) {
+		Visit(currentNode);
+		if (currentNode->leftChild)
+			q.push(currentNode->leftChild);
+		if (currentNode->rightChild)
+			q.push(currentNode->rightChild);
+		if (q.empty()) return;
+		currentNode = q.front(); // take one from the queue.
+		q.pop();
+	}
+}
+
+#endif
