cc4a9a097d60e2b1f3aeaa4f9ddd66af72ec3197
kutta97 <yhjin04170@gmail.com>
2021-05-30 17:13:57 +0900

    add prim.cpp

---
+++ dsdir10/prim.cpp
@@ -0,0 +1,48 @@
+// 금속조형디자인과 3학년
+// B743014 양혜진
+
+#include <fstream>
+#include <queue>
+#include "mstree.h"
+int NNODES; // #nodes in the graph is global
+
+priority_queue<Edge, vector<Edge>, Compare> PQ;
+
+queue<Edge> *Q; // each vertex has a queue
+
+void MoveIntoPQ_EdgesOfNode(int v) {
+	// Move all edges of vertex 'v' into PQ
+}
+
+void prim() {
+	Sets sets(NNODES);
+	int nedges = 0; // #edges found up to now
+	while (nedges < NNODES - 1) {
+		if (PQ.empty()) throw "No Spanning Tree Exists.";
+		Edge e = PQ.top(); PQ.pop();
+		int root0 = sets.Find(0); // 현재 선택된 노드들의 루트를 구한다.
+		// 꺼낸 e가 자격이 있으면 출력하고 e의 새로운 vertex에 대해 처리한다.
+	}
+}
+
+void ReadEdges4prim(istream& is) {
+	Q = new queue<Edge> [NNODES];
+	Edge e;
+	
+	while (GetEdge(is, NNODES, e)) {
+		// Q[e,v1]에 e를 넣고 Q[e,v2]에도 e를 넣는다
+	}
+	MoveIntoPQ_EdgesOfNode(0); // 0의 edges들을 PQ로 이동한다.
+}
+
+int main(int argc, char *argv[]) {
+	ifstream is;
+	if (argc==1) is.open("mstree.dat"); else is.open(argv[1]);
+	if (!is) { cerr << "No such input file\n"; return 1; }
+	is >> NNODES;
+	if (NNODES < 2) { cerr << "#nodes must ne 2.." << endl; return 1; }
+
+	try { ReadEdges4prim(is); prim(); }
+	catch (char const *str)
+		{ cerr << "Exception: " << str << endl; return 1; }
+}
