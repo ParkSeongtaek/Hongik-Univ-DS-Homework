ff7b9b63650b5cdc8ace00701138822ba4f7943a
kutta97 <yhjin04170@gmail.com>
2021-05-30 16:49:14 +0900

    add kruskal.cpp

--- dsdir10/kruskal.cpp
+++ dsdir10/kruskal.cpp
@@ -21,3 +21,20 @@ void kruskal() {
 		}
 	}
 }
+
+void ReadEdges4kruskal(istream& is) {
+	Edge e;
+	while (GetEdge(is, NNODES, e)) PQ.push(e);
+}
+
+int main(int argc, char *argv[]) {
+	ifstream is;
+	if (argc==1) is.open("mstree.dat"); else is.open(argv[1]);
+	if (!is) { cerr << "No such input file\n"; return 1; }
+	is >> NNODES;
+	if (NNODES < 2) { cerr << "#nodes must ne 2.." << endl; return 1; }
+
+	try { ReadEdges4kruskal(is); kruskal(); }
+	catch (char const *str)
+		{ cerr << "Exception: " << str << endl; return 1; }
+}
