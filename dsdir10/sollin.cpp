1cd3d994828bd4d1295543fa9cb5c28e41fab928
kutta97 <yhjin04170@gmail.com>
2021-06-02 05:11:16 +0900

    add sollin.cpp

---
+++ dsdir10/sollin.cpp
@@ -0,0 +1,36 @@
+// 금속조형디자인과 3학년
+// B743014 양혜진
+
+#include <fstream>
+#include <queue>
+#include <algorithm>
+#include "mstree.h"
+int NNODES; // #nodes in the graph is global
+
+priority_queue<Edge, vector<Edge>, Compare> *PQ;
+
+void sollin() {
+	
+}
+
+void ReadEdges4sollin(istream& is) {
+	PQ = new priority_queue<Edge, vector<Edge>, Compare> [NNODES];
+	Edge e;
+
+	while (GetEdge(is, NNODES, e)){
+		PQ[e.v1].push(e);
+		PQ[e.v2].push(e);
+	}
+}
+
+int main(int argc, char *argv[]) {
+	ifstream is;
+	if (argc==1) is.open("mstree.dat"); else is.open(argv[1]);
+	if (!is) { cerr << "No such input file\n"; return 1; }
+	is >> NNODES;
+	if (NNODES < 2) { cerr << "#nodes must ne 2.." << endl; return 1; }
+
+	try { ReadEdges4sollin(is); sollin(); }
+	catch (char const *str)
+		{ cerr << "Exception: " << str << endl; return 1; }
+}
